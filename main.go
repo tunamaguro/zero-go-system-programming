b216427f8c719a4dfa832efa386029eda4f63ff6
tunamaguro <tsu7magu6@gmail.com>
2025-01-04 16:32:04 +0000

    `bytes.Buffer`

--- main.go
+++ main.go
@@ -1,15 +1,13 @@
 package main
 
 import (
-	"os"
+	"bytes"
+	"fmt"
 )
 
 func main() {
-	file, err := os.Create("test.txt")
-	if err != nil {
-		panic(err)
-	}
-	file.Write([]byte("os.File example\n"))
-	file.Close()
+	var buffer bytes.Buffer
+	buffer.Write([]byte("bytes.Buffer example \n"))
+	fmt.Println((buffer.String()))
 
 }
