2ba0233e89b7a260dbf1214690cdfacf681be09f
tunamaguro <tsu7magu6@gmail.com>
2025-01-06 05:05:21 +0000

    udp

--- client.go
+++ client.go
@@ -1,66 +1,26 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"io"
 	"net"
-	"net/http"
-	"net/http/httputil"
-	"strconv"
 )
 
 func main() {
-	conn, err := net.Dial("tcp", "localhost:8888")
+	conn, err := net.Dial("udp4", "localhost:8888")
 	if err != nil {
 		panic(err)
 	}
 	defer conn.Close()
-	request, err := http.NewRequest(
-		"GET",
-		"http://localhost:8888",
-		nil)
+	fmt.Println("Sending to server")
+	_, err = conn.Write([]byte("Hello from Client"))
 	if err != nil {
 		panic(err)
 	}
-	err = request.Write(conn)
+	fmt.Println("Receiving from server")
+	buffer := make([]byte, 1500)
+	length, err := conn.Read(buffer)
 	if err != nil {
 		panic(err)
 	}
-	reader := bufio.NewReader(conn)
-	response, err := http.ReadResponse(reader, request)
-	if err != nil {
-		panic(err)
-	}
-	dump, err := httputil.DumpResponse(response, false)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(string(dump))
-	if len(response.TransferEncoding) < 1 ||
-		response.TransferEncoding[0] != "chunked" {
-		panic("wrong transfer encoding")
-	}
-	for {
-		// サイズを取得
-		sizeStr, err := reader.ReadBytes('\n')
-		if err == io.EOF {
-			break
-		}
-		// 16 進数のサイズをパース。サイズがゼロならクローズ
-		size, err := strconv.ParseInt(
-			string(sizeStr[:len(sizeStr)-2]), 16, 64)
-		if size == 0 {
-			break
-		}
-		if err != nil {
-			panic(err)
-		}
-		// サイズ数分バッファを確保して読み込み
-		line := make([]byte, int(size))
-		io.ReadFull(reader, line)
-		// `\r\nをスキップ`
-		reader.Discard(2)
-		fmt.Printf(" %d bytes: %s\n", size, string(line))
-	}
+	fmt.Printf("Received: %s\n", string(buffer[:length]))
 }
