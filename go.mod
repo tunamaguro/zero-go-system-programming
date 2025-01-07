04a94a67d02009c585e4e1851179b7f71c16ca66
tunamaguro <tsu7magu6@gmail.com>
2025-01-07 21:45:00 +0000

    mmap

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module helloworld
 go 1.23.4
 
 require (
+	github.com/edsrzf/mmap-go v1.2.0 // indirect
 	golang.org/x/sys v0.29.0 // indirect
 	gopkg.in/fsnotify/fsnotify.v1 v1.4.7 // indirect
 )
--- main.go
+++ main.go
@@ -2,22 +2,42 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"path/filepath"
+
+	"github.com/edsrzf/mmap-go"
 )
 
 func main() {
-	if len(os.Args) == 1 {
-		fmt.Printf("%s [exec file name]", os.Args[0])
-		os.Exit(1)
+	// テストデータを書き込み
+	var testData = []byte("0123456789ABCDEF")
+	var testPath = filepath.Join(os.TempDir(), "testdata")
+	err := os.WriteFile(testPath, testData, 0644)
+	if err != nil {
+		panic(err)
 	}
-	for _, path := range filepath.SplitList(os.Getenv("PATH")) {
-		execpath := filepath.Join(path, os.Args[1])
-		_, err := os.Stat(execpath)
-		if !os.IsNotExist(err) {
-			fmt.Println(execpath)
-			return
-		}
+	// メモリにマッピング
+	// m は []byte のエイリアスなので添字アクセス可能
+	f, err := os.OpenFile(testPath, os.O_RDWR, 0644)
+	if err != nil {
+		panic(err)
 	}
-	os.Exit(1)
+	defer f.Close()
+	m, err := mmap.Map(f, mmap.COPY, 0)
+	if err != nil {
+		panic(err)
+	}
+	defer m.Unmap()
+	// メモリ上のデータを修正して書き込む
+	m[9] = 'X'
+	m.Flush()
+	// 読み込んでみる
+	fileData, err := io.ReadAll(f)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("original: %s\n", testData)
+	fmt.Printf("mmap: %s\n", m)
+	fmt.Printf("file: %s\n", fileData)
 }
