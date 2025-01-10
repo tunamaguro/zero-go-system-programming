96fb733e5dc62c83b0daa746ad52f5fff9c3887a
tunamaguro <tsu7magu6@gmail.com>
2025-01-10 14:09:27 +0000

    sh

--- main.go
+++ main.go
@@ -2,42 +2,9 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"os"
-	"path/filepath"
-
-	"github.com/edsrzf/mmap-go"
 )
 
 func main() {
-	// テストデータを書き込み
-	var testData = []byte("0123456789ABCDEF")
-	var testPath = filepath.Join(os.TempDir(), "testdata")
-	err := os.WriteFile(testPath, testData, 0644)
-	if err != nil {
-		panic(err)
-	}
-	// メモリにマッピング
-	// m は []byte のエイリアスなので添字アクセス可能
-	f, err := os.OpenFile(testPath, os.O_RDWR, 0644)
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
-	m, err := mmap.Map(f, mmap.COPY, 0)
-	if err != nil {
-		panic(err)
-	}
-	defer m.Unmap()
-	// メモリ上のデータを修正して書き込む
-	m[9] = 'X'
-	m.Flush()
-	// 読み込んでみる
-	fileData, err := io.ReadAll(f)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("original: %s\n", testData)
-	fmt.Printf("mmap: %s\n", m)
-	fmt.Printf("file: %s\n", fileData)
+	fmt.Println(os.ExpandEnv("${HOME}/gobin"))
 }
