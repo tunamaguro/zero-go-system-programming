4b41bc00369870ba8a5082185d442f19a2ba8e1c
tunamaguro <tsu7magu6@gmail.com>
2025-01-27 13:51:27 +0000

    12.1.4

--- main.go
+++ main.go
@@ -3,16 +3,11 @@ package main
 import (
 	"fmt"
 	"os"
-
-	"golang.org/x/sys/unix"
 )
 
 func main() {
-	sid, err := unix.Getsid(os.Getpid())
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "エラー: %v\n", err)
-		return
-	}
-	fmt.Fprintf(os.Stderr, "グループID: %d セッションID: %d\n",
-		unix.Getpgrp(), sid)
+	fmt.Printf(" ユーザー ID: %d\n", os.Getuid())
+	fmt.Printf(" グループ ID: %d\n", os.Getgid())
+	groups, _ := os.Getgroups()
+	fmt.Printf(" サブグループ ID: %v\n", groups)
 }
