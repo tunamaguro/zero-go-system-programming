890e2ddfabfa8435b1b2c9a72bb8d08329005075
tunamaguro <tsu7magu6@gmail.com>
2025-02-02 14:58:21 +0000

    beos

--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.23.4
 require (
 	github.com/edsrzf/mmap-go v1.2.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	golang.org/x/sys v0.29.0 // indirect
 	gopkg.in/fsnotify/fsnotify.v1 v1.4.7 // indirect
 )
--- main.go
+++ main.go
@@ -2,11 +2,21 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
+
+	"github.com/mattn/go-colorable"
+	"github.com/mattn/go-isatty"
 )
 
+var data = "\033[34m\033[47m\033[4mB\033[31me\n\033[24m\033[30mOS\033[49m\033[m\n"
+
 func main() {
-	// 実装的には`PWD`環境変数を読んでるみたい
-	wd, _ := os.Getwd()
-	fmt.Println(wd)
+	var stdOut io.Writer
+	if isatty.IsTerminal(os.Stdout.Fd()) {
+		stdOut = colorable.NewColorableStdout()
+	} else {
+		stdOut = colorable.NewNonColorable(os.Stdout)
+	}
+	fmt.Fprintln(stdOut, data)
 }
